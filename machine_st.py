5f6589f9b4b4ad32847f39abccc947cbcc631256
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-12-24 17:54:05 +0530

    Update machine_st.py

--- machine_st.py
+++ machine_st.py
@@ -18,7 +18,7 @@ from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 
 # Load the saved model
-model = tf.keras.models.load_model(r"C:\Users\chand\OneDrive\Desktop\Movement Detection.v2i.clip\machine_model.h5")
+model = tf.keras.models.load_model(r"machine_model.h5")
 
 def main():
     # Set the layout
