20516d677a9f7526fc564de9985eef2e7e8f5ba4
subhabaha <51040380+subhabaha@users.noreply.github.com>
2023-12-26 05:51:01 +0530

    Update machine_st.py

--- machine_st.py
+++ machine_st.py
@@ -18,6 +18,7 @@ from datetime import datetime
 from streamlit_option_menu import option_menu
 import pytz
 from datetime import date
+import pywhatkit as wp
 
 def get_log(model, video): 
     frame_count = 0
@@ -109,6 +110,13 @@ def get_log(model, video):
 
         frame_count += 1
 
+        if status == "Running":
+            if current_day == "Saturday" or current_day == "Sunday":
+                wp.sendwhatmsg_instantly(phone_no = +918777625140, massage = "WARNING! Machine is running", wait_time = 10)
+            else:
+                #if current_time > 00:00:00:
+                    wp.sendwhatmsg_instantly(phone_no = +918777625140, massage = "WARNING! Machine is running", wait_time = 10)
+
     # Release the webcam capture object and close the OpenCV window
     cap.release()
 
@@ -197,6 +205,15 @@ def get_machine_status(model, video):
                 status_chk = 0
         count = 1
         frame_count += 1
+
+        st.write(current_time)
+
+        if status == "Running":
+            if current_day == "Saturday" or current_day == "Sunday":
+                wp.sendwhatmsg_instantly(phone_no = +918777625140, massage = "WARNING! Machine is running", wait_time = 10)
+            else:
+                #if current_time > 00:00:00:
+                    wp.sendwhatmsg_instantly(phone_no = +918777625140, massage = "WARNING! Machine is running", wait_time = 10)
 
     # Release the webcam capture object and close the OpenCV window
     cap.release()
