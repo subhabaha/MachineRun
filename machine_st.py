c2768208c0d2999137e1115b2b2ddc40b96cb041
subhabaha <51040380+subhabaha@users.noreply.github.com>
2024-01-04 13:26:31 +0530

    Update machine_st.py

--- machine_st.py
+++ machine_st.py
@@ -226,7 +226,7 @@ if selected == "Machine Status":
     # Display the machine status
     st.subheader("Machine Status")
     # Call the function to get the machine status and log
-    machine_status = get_machine_status(model, video_path)
+    get_machine_status(model, video_path)
 
 if selected == "Machine runtime log":
     # Display the machine status
