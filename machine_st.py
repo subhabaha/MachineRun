3824514b0227c2289d84beba8d2cf5b9c63b9cbc
subhabaha <51040380+subhabaha@users.noreply.github.com>
2024-01-04 17:37:36 +0530

    Update machine_st.py

--- machine_st.py
+++ machine_st.py
@@ -128,8 +128,10 @@ def get_machine_status(model, video):
     sender_email = "chandrapaulcs2001@gmail.com"
     sender_password = "lbjc utkq kawe fcvd"
     recipient = "chandrapauldas01@gmail.com"
+    subject = f"⚠️ Warning! Machine Status changed ⚠️"
     em['From'] = sender_email
     em['To'] = recipient
+    em['Subject'] = subject
     context = ssl.create_default_context()
     
     # Open a connection to the webcam (0 represents the default webcam)
@@ -199,9 +201,7 @@ def get_machine_status(model, video):
         if status_chk >= consecutive_frames_threshold:
             with clear.container():
                 st.write(f"Machine Status: {status}")
-                subject = f"⚠️ Warning! Machine is {status} ⚠️"
                 message = f"The Machine was observed {status} at {current_time} IST on {current_day}. \n\n *This is just a friendly reminder, Ignore if you think it is under the schedule*"
-                em['Subject'] = subject
                 em.set_content(message)
                 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
                     smtp.login(sender_email, sender_password)
