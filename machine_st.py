bc52feea6de6e555288497ac427cac7a4542f2e3
subhabaha <51040380+subhabaha@users.noreply.github.com>
2024-01-05 21:22:46 +0530

    Update machine_st.py

--- machine_st.py
+++ machine_st.py
@@ -57,7 +57,7 @@ def get_log(model, video):
             continue
 
         # Resize the frame to match the input size of the model
-        frame = cv2.resize(frame, (224, 224))
+        frame = cv2.resize(frame, (912, 912))
 
         # Preprocess the frame
         img_array = image.img_to_array(frame)
@@ -157,7 +157,7 @@ def get_machine_status(model, video):
             continue
 
         # Resize the frame to match the input size of the model
-        frame = cv2.resize(frame, (224, 224))
+        frame = cv2.resize(frame, (912, 912))
 
         # Preprocess the frame
         img_array = image.img_to_array(frame)
