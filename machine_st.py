1b7ead7e3ae50873016aace3cbe4b177f0f20895
subhabaha <51040380+subhabaha@users.noreply.github.com>
2024-01-06 00:46:09 +0530

    Update machine_st.py

--- machine_st.py
+++ machine_st.py
@@ -207,7 +207,7 @@ def get_machine_status(model, video):
 
         if status == "Running":
             chk_time = int(current_time[3:5])
-            if chk_time > chk_time_1 + 2 and flag_temp == 0:
+            if chk_time > chk_time_1 + 1 and flag_temp == 0:
                 message = f"The Machine was observed not running first at {chk_time_1_act} IST. It's been 1 minutues, and we have observed the machine is still not running. Current time is {current_time} IST. \n\n *This is just a friendly reminder, Ignore if you think it is under the schedule*"
                 em = EmailMessage()
                 em['From'] = sender_email
